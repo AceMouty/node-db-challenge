349b75e2a5ec4415f094e0efc9c78a8763fe1794
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-10-11 12:05:46 -0500

    setup get requests for the api

--- knexfile.js
+++ knexfile.js
@@ -8,12 +8,12 @@ module.exports = {
       filename: './data/dev.db3'
     },
     useNullAsDefault: true,
-    pool: {
-        afterCreate: (conn, done) => {
-         // runs after a connection is made to the sqlite engine
-         conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-      },
-    },
+    // pool: {
+    //     afterCreate: (conn, done) => {
+    //      // runs after a connection is made to the sqlite engine
+    //      conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+    //   },
+    // },
   },
 
   staging: {
